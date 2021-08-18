5cc49d056d50387050cdae82ad3a7d90df026c26
calliewentling <calliewentling@gmail.com>
2021-08-18 19:13:03 +0100

    profiles functional (minus global variable persistance), visualizing stories to prepare for instance saving

--- apregoar_site/apregoar/__init__.py
+++ apregoar_site/apregoar/__init__.py
@@ -9,9 +9,14 @@ from apregoar.models import Stories, UGazetteer, Instances, Users
 from sqlalchemy import text
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
+from flask_table import Table, Col
 import json
+import pandas as pd
+#import geopandas as gpd
+#import geojson
 
-global currentuser
+#global currentuser
+#global currentuid
 
 
 def create_app(test_config=None):
@@ -31,13 +36,19 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
+    
+    
     engine = create_engine('postgresql://postgres:thesis2021@localhost/postgres', echo=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
+#########################
+###### IT BEGINS ########
+#########################
 
-#### It's on
+#########################
+###### Initializing sessions
+#########################
 
     @app.before_request
     def create_sesion():
@@ -47,16 +58,25 @@ def create_app(test_config=None):
     def shutdown_session(response_or_exc):
         flask.g.session.commit()
         flask.g.session.close()
-  
+
+#########################
+###### Home
+#########################
         
     @app.route('/')
     def home():
         #return '<a href="/addstory"><button> Click here to publish articles </button></a>'
         return render_template("layout.html")
 
+#########################
+###### Profile
+#########################
+
     @app.route("/adduser")
     def adduser():
         return render_template("/profile/newuser.html")
+
+
 
     @app.route("/useradd", methods=['POST', 'GET'])
     def useradd():
@@ -85,6 +105,9 @@ def create_app(test_config=None):
 
         else:
             return render_template("profile/newuser.html")
+
+
+
         
     @app.route("/login", methods=['GET'])
     def login():
@@ -92,41 +115,71 @@ def create_app(test_config=None):
             SQL = text("SELECT username FROM apregoar.users")
             result = conn.execute(SQL)
             print(result)
-            unames=[]
+            usernames=[]
             for row in result:
-                unames.append(row[0])
-            print(unames)
+                usernames.append(row[0])
+
+            print("usernames: ",usernames)
         
-        return render_template("profile/login.html", unames=unames) 
+        return render_template("profile/login.html", unames=usernames) 
+
+
+
 
     @app.route("/confirmu", methods=["GET", "POST"])
     def confirmu():
+        
         if request.method == 'POST':
             print("arrived at confirmu")
+            global currentuser
             currentuser = request.form["selectuser"]
             print("current user is: "+currentuser)
+
+                    #Retrieve current user ID
+
+            with engine.connect() as conn:
+                SQL = text("SELECT u_id FROM apregoar.users WHERE username = :x")
+                SQL = SQL.bindparams(x=currentuser)
+                result = conn.execute(SQL)
+                print("results of query:")
+                print(result)
+                u_id=[]
+                for row in result:
+                    u_id.append(row[0])
+                print(u_id)
+                u_id=u_id[0]
+                print("u_id for recording with story: ",u_id)
+                global current_uid
+                current_uid=u_id
+                print("current_uid: ",current_uid)
+                print()
+
             return render_template("profile/profile.html", currentuser=currentuser)
         
         else:
             return render_template("profile/login.html")
 
-        
-        #else:
-        #    error = 1
-        #    return render_template("profile/newuser.html", error)
 
-
-
-        
-
+#########################
+###### Publish
+#########################
     
     @app.route("/addstory")
     def addstory():
         return render_template("publish/create.html")
 
 
+
     @app.route("/storyadd", methods=['POST'])
     def storyadd():
+        #solve this user issue -- apparently globals are staying global
+        #currentuser = "cwentling"
+        #current_uid=1
+        #title = "Jorge Romão, o artista que faz dos muros da graça telas para as suas pinturas"
+        print("")
+        print()
+        print("currentuser is: ",currentuser)
+        print()
         ##Required
         title = request.form["title"]
         pub_date = request.form["pubDate"]
@@ -137,20 +190,59 @@ def create_app(test_config=None):
         section = request.form["section"]
         tags = request.form["tags"]
         author = request.form["author"]
+        #Global value
+        u_id = current_uid
         #Prepare & Submit
-        entry = Stories(title, summary, pub_date, web_link, section, tags, author, publication)
+        entry = Stories(title, summary, pub_date, web_link, section, tags, author, publication, u_id)
         session.add(entry)
         session.commit()
+        return render_template("publish/review.html")
 
-        return render_template("publish/add_instance.html")
+
+
+    @app.route("/test", methods=['POST', 'GET'])
+    def test():
+        #solve this user issue -- apparently globals are staying global
+        currentuser = "cwentling"
+        current_uid=1
+        title = "Jorge Romão, o artista que faz dos muros da graça telas para as suas pinturas"
+        SQL = text("SELECT * FROM apregoar.stories WHERE stories.title = :x")
+        SQL = SQL.bindparams(x=title)
+        SQLt = text("SELECT * FROM apregoar.stories")
+
+        cstory_df = pd.read_sql(
+            SQLt, 
+            con=engine, 
+            #schema='apregoar', 
+            index_col='s_id', 
+            columns=['s_id', 'title', 'pub_date', 'web_link', 'publication', 'summary', 'section', 'tags', 'author', 'u_id'],
+            parse_dates=['pub_date']
+        )
+        cstory_df["pub_date"] = cstory_df['pub_date'].dt.strftime('%x')
+        print(cstory_df.head())
+        cstory=cstory_df.to_json(orient='records')
+        cstory=json.loads(cstory) #parsed
+        cstory=json.dumps(cstory)
+        print(cstory)
+
+        return render_template("publish/review.html", cstory=cstory)    
+
+
+
 
     @app.route("/add_instance")
     def addinstance():
         return render_template("publish/add_instance.html")
+
+
+
     
     @app.route("/viewmap")
     def viewmap():
         return render_template("publish/instance.html")
+
+
+
 
     @app.route("/localize")
     def localize():
