85c4ae8ed59c49434de516a26d8bae3f0a632e9c
calliewentling <calliewentling@gmail.com>
2022-02-28 21:45:12 +0000

    Added noticia review and finessed publisher dashboard

--- apregoar_thesis/app/publisher_views.py
+++ apregoar_thesis/app/publisher_views.py
@@ -43,12 +43,20 @@ users = {} #is this necessary
 @app.before_request
 def before_request_func():
     #print("Test of before request")
-    with engine.connect() as conn:
-        SQL = text("SELECT * FROM apregoar.users")
-        #print(SQL)            
-        result = conn.execute(SQL)   
-        #print("SQL executed")
-        
+    #Load all users (valid for sign in)
+    try:
+        with engine.connect() as conn:
+            SQL = text("SELECT * FROM apregoar.users")
+            #print(SQL)            
+            result = conn.execute(SQL)   
+            #print("SQL executed")
+            
+            
+    except:
+        print("Error in connecting!")
+        feedback = f"Erro!"
+        flash(feedback, "danger")
+    else:
         for row in result:
             #print(row)
             #print("username:",row['username'])
@@ -59,6 +67,10 @@ def before_request_func():
                 "affiliation": row['organization'],
                 "email": row['email']
             }
+
+    finally:
+        conn.close()
+
 
         #print(users)
         #print("Checkpoint end connect")  
@@ -89,34 +101,44 @@ def sign_up():
         email = request.form.get("email")
         organization = request.form.get("affiliation")
         
-        with engine.connect() as conn:
-            print(username)
-            SQL = text("SELECT username FROM apregoar.users WHERE username = :x")
-            SQL = SQL.bindparams(x=username)
-            result = conn.execute(SQL)
+        try:
+            with engine.connect() as conn:
+                print(username)
+                SQL = text("SELECT username FROM apregoar.users WHERE username = :x")
+                SQL = SQL.bindparams(x=username)
+                result = conn.execute(SQL)
+        except:
+            print("Error in validating unique username")
+            feedback=f"Erro"
+            flash(feedback, "danger")
+        else:
             count = 0
             for row in result:
                 print("username:",row['username'])
                 count += 1
-
             print(count)
-        
-        if count == 0:
-            con = psycopg2.connect("dbname=postgres user=postgres password=thesis2021")
-            cur = con.cursor()
-            cur.execute("""
-                INSERT INTO apregoar.users (username, password, organization, email)
-                VALUES (%(username)s,%(password)s,%(organization)s,%(email)s)
-                """,
-                {'username':username,'password':password, 'organization':organization, 'email':email}
-            )
-            con.commit()
-            print("User added to database")
-            return redirect(url_for("sign_in"))
-            #return render_template("user/sign_in.html")
-        else:
-            feedback=f"This username is already in use. Please select a new username."
-            return render_template("publisher/sign_up.html", feedback=feedback)
+            if count == 0:
+                con = psycopg2.connect("dbname=postgres user=postgres password=thesis2021")
+                try:
+                    with con:
+                        with con.cursor() as cur:
+                            cur.execute("""
+                                INSERT INTO apregoar.users (username, password, organization, email)
+                                VALUES (%(username)s,%(password)s,%(organization)s,%(email)s)
+                                """,
+                                {'username':username,'password':password, 'organization':organization, 'email':email}
+                            )
+                            con.commit()
+                except:
+                    print("Error in saving new user")
+                    feedback=f"Erro"
+                    flash(feedback, "danger")
+                else:
+                    print("User added to database")
+                    return redirect(url_for("sign_in"))
+            else:
+                feedback=f"This username is already in use. Please select a new username."
+                return render_template("publisher/sign_up.html", feedback=feedback)
     return render_template("publisher/sign_up.html")
 
 @app.route("/publisher/sign-in", methods=["GET", "POST"])
@@ -128,20 +150,21 @@ def sign_in():
         password = req.get("password")
         print("Entered username: ", username)
         print("Entered password: ", password)
-
-        with engine.connect() as conn:
-            SQL = text("SELECT * FROM apregoar.users WHERE username = :x and password = :y")
-            SQL = SQL.bindparams(x=username, y=password)
-            print(SQL)
-            result = conn.execute(SQL)   
-            print("SQL executed")
-            active_user = {}
-                
+        try:
+            with engine.connect() as conn:
+                SQL = text("SELECT * FROM apregoar.users WHERE username = :x and password = :y")
+                SQL = SQL.bindparams(x=username, y=password)
+                print(SQL)
+                result = conn.execute(SQL)   
+                print("SQL executed")
+        except:
+            print("Error in validating username password combo")
+            feedback = f"Erro"
+            flash(feedback,"danger")
+        else:
+            user = {}
             for row in result:
-                print(row)
-                print("username:",row['username'])
-                print("affiliation:",row['organization'])
-                active_user = {
+                user = {
                     username: {
                         "username": row['username'],
                         "affiliation": row['organization'],
@@ -149,38 +172,39 @@ def sign_in():
                         "id": row['u_id']
                     }
                 }
-                print("users dict: ",active_user)
+                print("users dict: ",user)
             print("Checkpoint end connect")  
 
 
-        #while result is not None: #Not using because result is always "not None", even if empty
-        if username in active_user:
-            print("Checkpoint results")
-            #print(result[0])
-            g.username = username
-            fsession['username'] = username #!!!!
-            print(fsession['username'])
-            #session.modified = True
-            print(fsession)
-            print("Session user assigned")
-            #return redirect(url_for("publisher/my_profile"))
-            return redirect(url_for("pub_profile"))    
-        else:
-            print("Combo not found")
-            feedback = f"Username/password combination not found. Please try again."
-            return render_template("publisher/sign_in.html", feedback=feedback)
+            #while result is not None: #Not using because result is always "not None", even if empty
+            if username in user:
+                print("Checkpoint results")
+                #print(result[0])
+                #g.username = username
+                fsession['username'] = username #!!!!
+                fsession['u_id'] = user[username]["id"]
+                fsession['org'] = user[username]["affiliation"]
+                fsession['email'] = user[username]["email"]
+                print("fsession: ",fsession)
+                #session.modified = True
+                print("Session user assigned")
+                #return redirect(url_for("publisher/my_profile"))
+                return redirect(url_for("pub_profile"))    
+            else:
+                print("Combo not found")
+                feedback = f"Username/password combination not found. Please try again."
+                return render_template("publisher/sign_in.html", feedback=feedback)
 
     return render_template("publisher/sign_in.html")
 
 @app.route("/publisher/my_profile")
 def pub_profile():
     if not fsession.get("username") is None:
-        activeu = fsession.get("username")
         user = {
-            "username": activeu,
-            "email": users[activeu]["email"],
-            "org": users[activeu]["affiliation"], 
-            "id": users[activeu]["id"]
+            "username": fsession["username"],
+            "email": fsession["email"],
+            "org": fsession["org"], 
+            "id": fsession["u_id"]
         }
         print("User id: ",user["id"])
 
@@ -192,6 +216,10 @@ def pub_profile():
 @app.route("/sign-out")
 def sign_out():
     fsession.pop("username", None)
+    fsession.pop("email", None)
+    fsession.pop("id", None)
+    fsession.pop("org", None)
+    print("fsession: ",fsession)
     return redirect(url_for("sign_in"))
 
 @app.route("/profile/<username>")
@@ -208,18 +236,57 @@ def profile(username):
 @app.route("/publisher/dashboard")
 def publisher_dashboard():
     if not fsession.get("username") is None:
-        activeu = fsession.get("username")
-        user = {
-            "username": activeu,
-            "email": users[activeu]["email"],
-            "org": users[activeu]["affiliation"], 
-            "id": users[activeu]["id"]
-        }
-        print("User id: ",user["id"])
+        print(fsession["username"])
 
-        return render_template("publisher/dashboard.html", username=user["username"])
+        try:
+            with engine.connect() as conn:
+                SQL = text("SELECT * FROM apregoar.stories WHERE u_id = :x")
+                SQL = SQL.bindparams(x=fsession["u_id"])
+                result = conn.execute(SQL)
+        except:
+            print("Error in identifying stories from user")
+            feedback=f"Erro"
+            flash(feedback, "danger")
+        else:
+            user_stories = []
+            for row in result:
+                u_story = {
+                    row["s_id"] : {
+                        "title": row["title"],                            "date": row["pub_date"]
+                    } 
+                }
+                user_stories.append(u_story)
+        finally: conn.close()
+
+        try:
+            with engine.connect() as conn:
+                SQL = text("SELECT * FROM apregoar.stories WHERE publication = :x")
+                SQL = SQL.bindparams(x=fsession["org"])
+                result = conn.execute(SQL)
+        except:
+            print("Error in identifying stories from user publication")
+            feedback=f"Erro"
+            flash(feedback,"danger")
+        else:
+            org_stories = []
+            o_story = {}
+            for row in result:
+                o_story = {
+                    row["s_id"] : {
+                        "title": row["title"],
+                        "date": row["pub_date"]
+                    } 
+                }
+                org_stories.append(o_story)
+            print()
+            print("last added story: ",o_story)
+            print("all stories associated with this organization: ",org_stories)
+            print()
+            return render_template("publisher/dashboard.html", username=fsession["username"], organization=fsession["org"], userStories = user_stories, orgStories = org_stories)
     else:
         print("No username found in fsession")
+        feedback=f"Não há um user ativo."
+        flash(feedback,"danger")
         return redirect(url_for("sign_in"))
 
 @app.route("/publisher/profile")
@@ -228,83 +295,95 @@ def publisher_profile():
 
 @app.route("/addstory")
 def addstory():
-    with engine.connect() as conn:
-        SQL = text("SELECT web_link FROM apregoar.stories")
-        result = conn.execute(SQL)
+    return render_template("publisher/create.html")
+
+
+@app.route("/<s_id>/review")
+def review_e(s_id):
+    print({s_id})
+    try:
+        with engine.connect() as conn:
+            SQL = text("SELECT * FROM apregoar.stories WHERE s_id = :x")
+            SQL = SQL.bindparams(x=s_id)
+            result = conn.execute(SQL)
+    except:
+        print("Error in extracting desired story from database")
+        feedback=f"Erro"
+        flash(feedback,"danger")
+    else:
+        story = {}
+        for row in result:
+            story = row
+        print(story)
+        if story:
+            return render_template("publisher/review.html", story=story)
+    
+    return render_template("publisher/dashboard.html")
+
+
+@app.route("/review", methods=['POST'])
+def review():
+    print()
+    print("current user is: ",fsession["username"])
+    print("current user id is: ",fsession["id"])
+    print()
+
+    try:
+        with engine.connect() as conn:
+            SQL = text("SELECT web_link FROM apregoar.stories")
+            result = conn.execute(SQL)
+    except:
+        print("Error in extracting desired story from database")
+        feedback=f"Erro"
+        flash(feedback,"danger")
+    else:
         existing_urls = []
         for row in result:
             existing_urls.append(row["web_link"])
         print(existing_urls)
-    return render_template("publisher/create.html", urls=existing_urls)
-
-@app.route("/storyadd", methods=['POST'])
-def storyadd():
-    activeu = fsession.get("username")
-    print("")
-    print()
-    print("current user is: ",activeu)
-    u_id = users[activeu]["id"]
-    u_org = users[activeu]["affiliation"]
-    print("current user id is: ",u_id)
-    print()
     
-    story = {
-        ##Required
-        "title": request.form["title"],
-        "pub_date": request.form["pubDate"],
-        "web_link": request.form["webLink"],
-        "publication": request.form["publication"],
-        ##Optional 
-        "summary": request.form["summary"],
-            #Move these to review?
-        "section" : request.form["section"],
-        "tags": request.form["tags"],
-        "author": request.form["author"]
-    }
+        story = {
+            ##Required
+            "title": request.form["title"],
+            "pub_date": request.form["pubDate"],
+            "web_link": request.form["webLink"],
+            "publication": request.form["publication"],
+            ##Optional 
+            "summary": request.form["summary"],
+                #Move these to review?
+            "section" : request.form["section"],
+            "tags": request.form["tags"],
+            "author": request.form["author"]
+        }
 
-    if story["web_link"] in existing_urls:
-        feedback = f"A história já existe no database: "+story["web_link"]
-        flash(feedback, "warning")
-        return render_template("publisher/create.html")
-    
-    return render_template("publisher/review.html", story=story)
-    #return render_template("publisher/localize.html")    
-            
-    '''
-    ##THIS MUST BE FIGURED OUT Pausing because first I need to redo the signin
-    #Global value
-    u_id = current_uid
-    #Prepare & Submit
-    entry = Stories(title, summary, pub_date, web_link, section, tags, author, publication, u_id)
-    session.add(entry)
-    session.commit()
-    #Retreive the s_id
-    with engine.connect() as conn:
-        SQL = text("SELECT s_id FROM apregoar.stories WHERE title = :x")
-        SQL = SQL.bindparams(x=title)
-        result = conn.execute(SQL)
-        for row in result:
-            s_id=row['s_id']
-            print(s_id)
-            global current_sid
-            current_sid=s_id
-    '''
-
-    '''
-    #Connect to database
-    with engine.connect() as conn:
-        SQL = text("SELECT s_id FROM apregoar.stories WHERE title =:t and publication =:p") #Should this be title per publication?
-        SQL = SQL.bindparams(t=story["title"], p=story["publication"])
-        result = conn.execute(SQL)
-        print("Result = ",result)
-        #If this story/publication combo already exists:
-        sametitle = []
-        for row in result:
-            sametitle.append(row["s_id"])
-        if len(sametitle) > 0:
-            s_ids = str(sametitle)
-            feedback = f"Já existe a manchete de '"+story["title"]+"': "+s_ids+"."
-            print(feedback)
+        if story["web_link"] in existing_urls:
+            feedback = f"A história já existe no database: "+story["web_link"]
             flash(feedback, "warning")
-    '''
+            return render_template("publisher/create.html")
+
+        #Prepare & Submit
+        con = psycopg2.connect("dbname=postgres user=postgres password=thesis2021")
+        try:
+            with con:
+                with con.cursor() as cur:
+                    cur.execute("""
+                        INSERT INTO apregoar.stories (title, summary, pub_date, web_link, section, tags, author, publication, u_id)
+                        VALUES (%(title)s,%(summary)s,%(pub_date)s,%(web_link)s,%(section)s, %(tags)s, %(author)s,%(publication)s,%(u_id)s)
+                        """,
+                        {'title':story["title"],'summary':story["summary"], 'pub_date':story["pub_date"], 'web_link': story["web_link"], 'section': story["section"], 'tags': story["tags"], 'author': story["author"], 'publication':story["publication"], 'u_id':fsession["u_id"]}
+                    )
+                    con.commit()
+                    print("Story added to database")
+            con.close()
+        except psycopg2.Error as e:
+            print(e.pgerror)
+            print(e.diag.message_primary)
+            feedback = f"Excepção: a história não ficou guardada. Erro: "+e.pgerror+", "+e.diag.message_primary
+            flash(feedback, "danger")
+            con.close()
+        else:
+            return render_template("publisher/review.html",story=story)
+    
+    return render_template("publisher/create.html")
+            
     
